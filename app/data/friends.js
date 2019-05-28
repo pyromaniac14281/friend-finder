2553ff75b50c62a53270098103152c3a86dff247
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-05-27 23:10:44 -0400

    permanent user data for matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArray = [
   {
-    name: "Scooter",
+    name: "Scooter", 
     photo:
       "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
     scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"]
